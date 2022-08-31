b273a30bb7ffa103b02530ffb462e4486b7d6987
Guolong Li <Guolong.LiRealtor@gmail.com>
2022-08-31 09:05:58 -0500

    initial commit

---
+++ Variables/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string string1 = "abc";
+            int int1 = 8;
+            char char1 = '1';
+            bool bool1 = true;
+            double num1 = 1.5;
+            decimal num2 = 1.5m;
+            Console.WriteLine($"My dog's name is {string1}, He is {num1} years old"); 
+        }
+    }
+}
